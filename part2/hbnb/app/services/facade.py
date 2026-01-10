bc37b17c23ca79b8972dab287008c2a782ce9ada
Alanoud m <alanoud_alharthi@outlook.com>
2026-01-10 14:30:47 +0300

    Update facade.py

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -21,6 +21,43 @@ class HBnBFacade:
 
     def update_user(self, user_id, data):
         user = self.get_user(user_id)
+        from app.models.amenity import Amenity
+from app.services.repository import InMemoryRepository
+
+class HBnBFacade:
+    def __init__(self):
+        self.amenity_repo = InMemoryRepository()
+
+    def create_amenity(self, amenity_data):
+        name = amenity_data.get("name")
+        if not name:
+            raise ValueError("Amenity name is required")
+
+        amenity = Amenity(name=name)
+        self.amenity_repo.add(amenity)
+        return amenity
+
+    def get_amenity(self, amenity_id):
+        amenity = self.amenity_repo.get(amenity_id)
+        if not amenity:
+            return None
+        return amenity
+
+    def get_all_amenities(self):
+        return self.amenity_repo.get_all()
+
+    def update_amenity(self, amenity_id, amenity_data):
+        amenity = self.amenity_repo.get(amenity_id)
+        if not amenity:
+            return None
+
+        name = amenity_data.get("name")
+        if not name:
+            raise ValueError("Amenity name is required")
+
+        amenity.name = name
+        return amenity
+
         if not user:
             return None
         
