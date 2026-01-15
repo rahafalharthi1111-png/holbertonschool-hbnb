d8483b7fbdd7425086d0c71ab9b31b3f668281d3
shahad-FS <alsaneea.s@gmail.com>
2026-01-15 18:04:08 +0000

    configration file

---
+++ part3/hbnb/config.py
@@ -0,0 +1,9 @@
+import os
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
+    JWT_SECRET_KEY = SECRET_KEY
+    DEBUG = False
+
+class DevelopmentConfig(Config):
+    DEBUG = True
