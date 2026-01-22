7100760eb783868b5696beb132b4372221debc80
Shahad Alsaneea <alsaneea.s@gmail.com>
2026-01-23 01:14:55 +0300

    Update app initialization and namespace registration
    Refactor API namespace registration and initialization.

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -1,43 +1,37 @@
 from flask import Flask
-from flask_bcrypt import Bcrypt
-from flask_restx import Api
-from flask_jwt_extended import JWTManager
-
-from config import DevelopmentConfig
-
-from app.services.facade import HBnBFacade
-from app.models.user import User
+import config
+from app.extensions import rest_api, bcrypt, jwt
 from app import db
 
 
-bcrypt = Bcrypt()
-jwt = JWTManager()
 
 
-def create_app(config_class=DevelopmentConfig):
+
+def create_app(config_class=config.DevelopmentConfig):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
-
+    rest_api.init_app(app)
     bcrypt.init_app(app)
     jwt.init_app(app)
     db.init_app(app)
 
-    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
-    from app.api.v1.users import api as users_ns
-    from app.api.v1.auth_ns import api as auth_ns
-    from app.api.v1.protected_ns import api as protected_ns
-    from app.api.v1.amenities import api as amenities_ns
-    from app.api.v1.places import api as places_ns
-    from app.api.v1.reviews import api as reviews_ns
-
     
-    api.add_namespace(users_ns, path='/api/v1/users')
-    api.add_namespace(auth_ns, path='/api/v1/auth_ns')
-    api.add_namespace(protected_ns, path='/api/v1/protected_ns')
-    api.add_namespace(amenities_ns, path='/api/v1/amenities')
-    api.add_namespace(places_ns, path="/api/v1/places")
-    api.add_namespace(reviews_ns, path="/api/v1/reviews")
+    from app.api.v1.users import ns as users_ns
+    from app.api.v1.amenities import ns as amenities_ns
+    from app.api.v1.places import ns as places_ns
+    from app.api.v1.reviews import ns as reviews_ns
+    from app.api.v1.login import ns as login_ns
+    from app.api.v1.admin import ns as admin_ns
+
+    # Register the namespace
+
+    rest_api.add_namespace(users_ns, path='/api/v1/users')
+    rest_api.add_namespace(amenities_ns, path='/api/v1/amenities')
+    rest_api.add_namespace(places_ns, path="/api/v1/places")
+    rest_api.add_namespace(reviews_ns, path="/api/v1/reviews")
+    rest_api.add_namespace(login_ns, path= '/api/v1/auth')
+    rest_api.add_namespace(admin_ns, path='/api/v1/admin')
     
 
     return app
