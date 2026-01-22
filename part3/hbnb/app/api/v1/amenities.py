28e0c12aea406c7f50c0c458a8ff6cf3dca429dd
Shahad Alsaneea <alsaneea.s@gmail.com>
2026-01-23 01:16:34 +0300

    Refactor amenities API to use new namespace

--- part3/hbnb/app/api/v1/amenities.py
+++ part3/hbnb/app/api/v1/amenities.py
@@ -1,30 +1,23 @@
 from flask_restx import Namespace, Resource, fields
-from flask_jwt_extended import jwt_required, get_jwt
 from app.services import facade
 
-api = Namespace('amenities', description='Amenity operations')
+ns = Namespace('amenities', description='Amenity operations')
 
-amenity_model = api.model('Amenity', {
+amenity_model = ns.model('Amenity', {
     'name': fields.String(required=True, description='Name of the amenity')
 })
 
 
 
-@api.route('/')
+@ns.route('/')
 class AmenityList(Resource):
-    @jwt_required()
-    @api.expect(amenity_model)
-    @api.response(201, 'Amenity successfully created')
-    @api.response(403, 'Admin privileges required')
-    @api.response(400, 'Invalid input data')
+
+    @ns.expect(amenity_model)
+    @ns.response(201, 'Amenity successfully created')
+    @ns.response(400, 'Invalid input data')
     def post(self):
         """Register a new amenity"""
-        claims = get_jwt()
-
-        if not claims.get('is_admin', False):
-            return {'error': 'Admin privileges required'}, 403
-
-        data = api.payload
+        data = ns.payload
         amenity = facade.create_amenity(data)
 
         if not amenity:
@@ -32,7 +25,7 @@ class AmenityList(Resource):
 
         return amenity.to_dict(), 201
 
-    @api.response(200, 'List of amenities retrieved successfully')
+    @ns.response(200, 'List of amenities retrieved successfully')
     def get(self):
         """Retrieve all amenities"""
         amenities = facade.get_all_amenities()
@@ -40,11 +33,11 @@ class AmenityList(Resource):
     
 
 
-@api.route('/<amenity_id>')
+@ns.route('/<amenity_id>')
 class AmenityResource(Resource):
 
-    @api.response(200, 'Amenity details retrieved successfully')
-    @api.response(404, 'Amenity not found')
+    @ns.response(200, 'Amenity details retrieved successfully')
+    @ns.response(404, 'Amenity not found')
     def get(self, amenity_id):
         """Get amenity by ID"""
         amenity = facade.get_amenity(amenity_id)
@@ -54,20 +47,13 @@ class AmenityResource(Resource):
 
         return amenity.to_dict(), 200
 
-
-    @jwt_required()
-    @api.expect(amenity_model)
-    @api.response(200, 'Amenity updated successfully')
-    @api.response(403, 'Admin privileges required')
-    @api.response(404, 'Amenity not found')
-    @api.response(400, 'Invalid input data')
+    @ns.expect(amenity_model)
+    @ns.response(200, 'Amenity updated successfully')
+    @ns.response(404, 'Amenity not found')
+    @ns.response(400, 'Invalid input data')
     def put(self, amenity_id):
-        claims = get_jwt()
-
-        if not claims.get('is_admin', False):
-            return {'error': 'Admin privileges required'}, 403
-
-        data = api.payload
+        """Update an amenity"""
+        data = ns.payload
         result = facade.update_amenity(amenity_id, data)
 
         if result is None:
