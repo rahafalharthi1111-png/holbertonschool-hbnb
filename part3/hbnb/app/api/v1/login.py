0f76f590ce73149bd1d1714ff1c603f619657e59
Shahad Alsaneea <alsaneea.s@gmail.com>
2026-01-23 01:17:22 +0300

    Refactor login endpoint to use new namespace

---
+++ part3/hbnb/app/api/v1/login.py
@@ -0,0 +1,36 @@
+from flask_restx import Namespace, Resource, fields
+from flask_jwt_extended import create_access_token
+from app.services import facade
+
+ns = Namespace("auth", description='Authintication operations')
+
+
+# Model for input validation
+login_model = ns.model('Login',{
+    "email": fields.String(
+        required=True,
+        description='User emal'
+    ),
+    'password': fields.String(
+        required=True,
+        description='User password'
+    )
+})
+@ns.route('/login')
+class Login(Resource):
+    @ns.expect(login_model)
+    @ns.response(401, 'Invalid credentials')
+    @ns.response(200, 'successfuly loged in')
+    def post(self):
+        credentials = ns.payload
+
+        user = facade.get_user_by_email(credentials['email'])
+        if not user or not user.verify_password(credentials['password']):
+            return {'error': 'Invalid credentials'}, 401
+        
+        access_token = create_access_token(
+            identity=str(user.id),
+            additional_claims={'is_admin': user.is_admin}
+        )
+
+        return {'access_token': access_token}, 200
