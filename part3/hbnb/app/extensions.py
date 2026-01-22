d4b1e4133c82e171926e9343c5832d4a55300cc0
Shahad Alsaneea <alsaneea.s@gmail.com>
2026-01-23 01:10:39 +0300

    Add Flask extensions for API and authentication
    Initialize Flask extensions for API, authentication, and security.

---
+++ part3/hbnb/app/extensions.py
@@ -0,0 +1,23 @@
+from flask_restx import Api
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+
+authorizations = {
+    'BearerAuth': {
+        'type': 'apiKey',
+        'in': 'header',
+        'name': 'Authorization',
+        'description': 'Bearer <JWT>'
+    }
+}
+
+rest_api = Api(version='1.0',
+        title='HBnB API',
+        description='HBnB Application API',
+        doc='/api/v1/',
+        authorizations=authorizations,
+        security='BearerAuth'
+        )
+
+bcrypt = Bcrypt()
+jwt = JWTManager()
