4c61e3868ac4735bb2e7ad6726a7c0dd66eb759f
Shahad Alsaneea <alsaneea.s@gmail.com>
2026-01-23 01:29:11 +0300

    Implement user validation and update method
    Added validation for first name, last name, and email format. Updated user_update method to call validate after setting user attributes.

--- part3/hbnb/app/models/user.py
+++ part3/hbnb/app/models/user.py
@@ -11,16 +11,37 @@ class User(BaseModel):
     password_hash = db.Column(db.String(128), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
 
-    def set_password(self, password):
+ def validate(self):
+        # validate that first name is not empty or " "
+        if not self.first_name or not self.first_name.strip():
+            raise ValueError("First name is required")
+
+        # validate that last name is not empty or " "
+        if not self.last_name or not self.last_name.strip():
+            raise ValueError("Last name is required")
+
+        # validate that the email match emails patterns
+        if not re.match(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", self.email):
+            raise ValueError("Invalid email format")
+        
+    def user_update(self, data):
+        if 'first_name' in data:
+            self.first_name = data['first_name'][:50].strip()
+        if 'last_name' in data:
+            self.last_name = data['last_name'][:50].strip()
+        if 'email' in data:
+            self.email = data['email'].lower().strip()
+        self.validate()
+
+
+    def hash_password(self, password):
         if not password:
             raise ValueError("Password is required")
+        
+        from app.extensions import bcrypt
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
 
-        self.password_hash = bcrypt.generate_password_hash(
-            password
-        ).decode("utf-8")
 
-    def check_password(self, password):
-        return bcrypt.check_password_hash(
-            self.password_hash,
-            password
-        )
+    def verify_password(self, password):
+        from app.extensions import bcrypt
+        return bcrypt.check_password_hash(self.password, password)
