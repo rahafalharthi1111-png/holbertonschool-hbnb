67a6954f65583b6c9d9f68f6bbfd3b29bb28aa5e
Shahad Alsaneea <alsaneea.s@gmail.com>
2026-01-24 19:55:26 +0300

    Initialize models module with imports and __all__

---
+++ part3/hbnb/app/models/__init__.py
@@ -0,0 +1,6 @@
+from app.models.amenity import Amenity
+from app.models.place import Place
+from app.models.review import Review
+from app.models.user import User
+
+__all__ = ["Amenity", "Place", "Review", "User"]
