236e4b07a358049271790e3a4397e2f1142c6006
Shahad Alsaneea <alsaneea.s@gmail.com>
2026-01-24 19:59:41 +0300

    Refactor repositories to use SQLAlchemyRepository

--- part3/hbnb/app/services/facade.py
+++ part3/hbnb/app/services/facade.py
@@ -1,18 +1,14 @@
-from app.persistence.user_repository import UserRepository
-from app.persistence.repository import InMemoryRepository
-from app.models.user import User
-from app.models.amenity import Amenity
-from app.models.place import Place
-from app.models.review import Review
+from app.persistence import SQLAlchemyRepository, UserRepository
+from app.models import User, Amenity, Place, Review
 
 
 
 class HBnBFacade:
     def __init__(self):
         self.user_repo = UserRepository()
-        self.amenity_repo = InMemoryRepository()
-        self.place_repo = InMemoryRepository()
-        self.review_repo = InMemoryRepository()
+        self.amenity_repo = SQLAlchemyRepository(Amenity)
+        self.place_repo = SQLAlchemyRepository(Place)
+        self.review_repo = SQLAlchemyRepository(Review)
 
         #Users CRUD
     def create_user(self, user_data):
