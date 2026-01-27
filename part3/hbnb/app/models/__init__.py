91a45d284191675421ef39f4bd7648b87b0e9eba
Shahad Alsaneea <alsaneea.s@gmail.com>
2026-01-27 22:03:12 +0300

    Add BaseModel to __all__ exports in init.py

--- part3/hbnb/app/models/__init__.py
+++ part3/hbnb/app/models/__init__.py
@@ -1,6 +1,8 @@
+from app.models.base_model import BaseModel
 from app.models.amenity import Amenity
 from app.models.place import Place
 from app.models.review import Review
 from app.models.user import User
 
-__all__ = ["Amenity", "Place", "Review", "User"]
+__all__ = ["BaseModel", "Amenity", "Place", "Review", "User"]
+
