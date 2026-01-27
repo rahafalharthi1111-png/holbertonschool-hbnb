52ce84ef681c20600e21ebde106398c532ff17af
Shahad Alsaneea <alsaneea.s@gmail.com>
2026-01-27 22:05:30 +0300

    Update user.py

--- part3/hbnb/app/models/user.py
+++ part3/hbnb/app/models/user.py
@@ -1,18 +1,21 @@
-from app import db, bcrypt
+from app.extensions import db, bcrypt
 from app.models.base_model import BaseModel
 from sqlalchemy.orm import validates
 import re
 
-
-
 class User(BaseModel):
     __tablename__ = "users"
 
+    #Columns
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(128), unique=True, nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
     password = db.Column(db.String(128), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
+
+    #Relastionships
+    places = db.relationship("Place", backref='owner', lazy=True)
+    reviews = db.relationship("Review", backref='auth', lazy=True)
 
     def __init__(self, first_name, last_name, email, password, is_admin=False):
         self.first_name = first_name.strip()[:50]
