a7a8d76c727a0808358d0b264c4ab0a228876689
Shahad Alsaneea <alsaneea.s@gmail.com>
2026-01-27 22:06:00 +0300

    Update __init__.py

--- part3/hbnb/app/persistence/__init__.py
+++ part3/hbnb/app/persistence/__init__.py
@@ -1,2 +1,5 @@
 from app.persistence.repository import SQLAlchemyRepository
 from app.persistence.user_repository import UserRepository
+from app.persistence.place_repository import PlaceRepository
+from app.persistence.review_repository import ReviewRepository
+from app.persistence.amenity_repository import AmenityRepository
