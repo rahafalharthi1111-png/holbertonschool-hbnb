b963acd5d95df1c2afcead3657a4db55375257c3
Shahad Alsaneea <alsaneea.s@gmail.com>
2026-01-27 22:03:32 +0300

    Update Amenity model to validate name length
    Changed the maximum length of the 'name' field to 50 characters and added validation.

--- part3/hbnb/app/models/amenity.py
+++ part3/hbnb/app/models/amenity.py
@@ -1,7 +1,17 @@
-from app.extensions import db
-from app.models.base_model import BaseModel
+from sqlalchemy.orm import validates
 
-class Amenity(BaseModel, db.Model):
+from app.models import BaseModel
+from app.extensions import db
+
+
+
+class Amenity(BaseModel):
     __tablename__ = "amenities"
 
-    name = db.Column(db.String(128), nullable=False, unique=True)
+    name = db.Column(db.String(50), nullable=False)
+
+    @validates("name")
+    def validate_name(self, key, value):
+        if not value or len(value) > 50:
+            raise ValueError("Amenity name is required and max 50 chars.")
+        return value
