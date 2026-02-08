5e26aa41b3a0000ba4da96e9814afeff63392255
Shahad Alsaneea <alsaneea.s@gmail.com>
2026-02-08 17:14:07 +0300

    Add CORS support and clean up imports

--- part4/hbnb/app/extensions.py
+++ part4/hbnb/app/extensions.py
@@ -1,7 +1,8 @@
 from flask_restx import Api
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
-from flask_sqlalchemy import SQLAlchemy 
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 authorizations = {
     'BearerAuth': {
